57023ca0993371588faad020ec71d9c5dbf4c07e
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2023-02-23 22:26:04 +0800

    interface

--- 02-js_webpack-master/src/ch06-class.ts
+++ 02-js_webpack-master/src/ch06-class.ts
@@ -39,3 +39,66 @@ console.log(u2);
 console.log(u3);
 
 
+// implement 與 interface
+// 如果做個會員系統需要以上面的為基礎，至少要包含的功能、屬性
+interface OBJ {
+    name:string
+}
+const obj: OBJ = { name:'' }
+interface UserInterface {
+    id: number
+    name: string
+    age: number
+    address: string
+    // 會員功能
+    // add 需要接收參數
+    add: (data: any) => void     // 不會有回傳的功能
+    update: (id: number) => boolean   // 是否更新成功
+    delete: (id: number) => boolean   // 是否刪除成功
+} 
+
+// 建立一個 User 的功能但要增加直播的功能
+class LiveUser implements UserInterface {
+    // 基本的功能
+    id: number
+    name: string
+    age: number
+    address: string
+
+    add(data: any) {}
+    update(id: number) {
+        // ...
+        return true
+    }
+    delete(id: number) {
+        // ...
+        return true
+    }
+
+    // 額外的功能
+    startLive() {}
+    endLive() {}
+}
+
+// 建立一個 User 的功能但要增加影片的功能
+class VideoUser implements UserInterface {
+    // 基本的功能
+    id: number
+    name: string
+    age: number
+    address: string
+
+    add(data: any) {}
+    update(id: number) {
+        // ...
+        return true
+    }
+    delete(id: number) {
+        // ...
+        return true
+    }
+
+    // 額外的功能
+    postVideo() {}
+    deleteVideo() {}
+}
