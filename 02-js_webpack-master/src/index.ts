b354a74b6f08f78c0567351eec0dcae7d11e6f3e
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2023-03-15 22:28:27 +0800

    update

--- 02-js_webpack-master/src/index.ts
+++ 02-js_webpack-master/src/index.ts
@@ -1,10 +1,12 @@
 import "./index.css";
 
+import '@/utils/parsing'
+
 // 路徑引入
-import './ch08-commonJS'
+// import './ch08-commonJS'
 
 // npm 引入
-import 'webpack'
+// import 'webpack'
 
 // import "./ch03-basic.ts";
 // import "./ch04-type_and_interface_and_emus.ts";
@@ -14,26 +16,26 @@ import 'webpack'
 
 
 
-import "./ch08-commonJS";
-const data = require('./ch08-commonJS') // 拿到 userName1
-// require 是動態引入
-if(true){
-    // 這裡是可以使用的
-    const data = require('./ch08-commonJS')
-}
+// import "./ch08-commonJS";
+// const data = require('./ch08-commonJS') // 拿到 userName1
+// // require 是動態引入
+// if(true){
+//     // 這裡是可以使用的
+//     const data = require('./ch08-commonJS')
+// }
 
 
-import "./ch08-esModules";
-// 基本輸出
-// import { userName, age } from './ch08-esModules'
-// 進階輸出   ↓ 給個別名
-import * as all from './ch08-esModules'
-console.log(all);
+// import "./ch08-esModules";
+// // 基本輸出
+// // import { userName, age } from './ch08-esModules'
+// // 進階輸出   ↓ 給個別名
+// import * as all from './ch08-esModules'
+// console.log(all);
 
-// 進階輸出 預設輸出          ↓ 給個別名，如果參數有衝突
-import dataES, {userName as name1, age, address} from './ch08-esModules'
+// // 進階輸出 預設輸出          ↓ 給個別名，如果參數有衝突
+// import dataES, {userName as name1, age, address} from './ch08-esModules'
 
-console.log(dataES);
+// console.log(dataES);
 // import 是靜態引入，寫越上面越好
 
 
