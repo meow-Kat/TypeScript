dd2e8fae3ce6d1b071f8df90a2689f5ffdd927f3
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2023-04-05 22:18:27 +0800

    update TS project

--- 03-nodejs_project_template-master/src/client/pages/chatRoom/index.ts
+++ 03-nodejs_project_template-master/src/client/pages/chatRoom/index.ts
@@ -1,4 +1,14 @@
 import "./index.css";
+
+import { io } from 'socket.io-client'    // 引入 client 的 package
+
 import { name } from "@/utils";
 
 console.log("client side chatroom page", name);
+
+
+// 建立連接到 node sever
+const clientIo = io()
+clientIo.on('join', msg => {
+    console.log('msg');
+})
--- 03-nodejs_project_template-master/src/index.ts
+++ 03-nodejs_project_template-master/src/index.ts
@@ -2,11 +2,25 @@ import devServer from "./server/dev";
 import prodServer from "./server/prod";
 import express from "express";    // node.js 後端框架
 
+import { Server } from 'socket.io'  // 引入 socket.io
+import http from 'http';
+
 import { name } from "@/utils";
 
 const port = 3000;
 // 啟動後端框架拿到內容
 const app = express();
+// 改造 http
+const server = http.createServer(app)
+const io = new Server(server)   // server 端
+
+
+// 監測連接
+io.on('connection', socket => {
+  // join 的頻道 ↓      ↓ 發送訊息
+  socket.emit('join', 'welcome')
+})
+
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
 if (process.env.NODE_ENV === "development") {
@@ -17,6 +31,6 @@ if (process.env.NODE_ENV === "development") {
 
 console.log("server side", name);
 // 啟動伺服器渲染在這個 port 上
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`The application is running on port ${port}.`);
 });
