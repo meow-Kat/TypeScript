7f29362e1175434476b8f86d702636ef896fbe08
meow-Kat <76527080+meow-Kat@users.noreply.github.com>
2023-04-06 21:48:27 +0800

    update

--- 03-nodejs_project_template-master/src/client/pages/chatRoom/index.ts
+++ 03-nodejs_project_template-master/src/client/pages/chatRoom/index.ts
@@ -2,10 +2,17 @@ import "./index.css";
 
 import { io } from 'socket.io-client'    // 引入 client 的 package
 
-import { name } from "@/utils";
+// 跳轉 chat room 在 url 上取得資料
+const url = new URL(location.href)
+const userName = url.searchParams.get('user_name')
+const roomName = url.searchParams.get('room_name')
 
-console.log("client side chatroom page", name);
+// 如果沒有 userName 或 roomName 回到 main.html
+if(!userName || !roomName){
+    location.href = 'main/main.html'
+}
 
+console.log(url,userName,roomName);
 
 // 建立連接到 node sever
 const clientIo = io()
--- 03-nodejs_project_template-master/src/client/pages/main/index.ts
+++ 03-nodejs_project_template-master/src/client/pages/main/index.ts
@@ -1,4 +1,16 @@
 import "./index.css";
-import { name } from "@/utils";
 
-console.log("client side main page", name);
+// 其實會自動推斷類型
+const nameInput = document.querySelector('#nameInput') as HTMLInputElement
+const roomSelect = document.querySelector('#roomSelect') as HTMLSelectElement
+const startBtn = document.querySelector('#startBtn') as HTMLButtonElement
+
+startBtn.addEventListener('click', () => {
+    const userName = nameInput.value
+    const roomName = roomSelect.value
+
+    // console.log(userName, roomName);
+
+    location.href = `/chatRoom/chatRoom.html?user_name=${userName}&room_name=${roomName}`
+    
+})
